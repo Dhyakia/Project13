e33e6f2c80cf185d6b1050efaba4abee9ba9a391
Rimonteil <nardiotencha@gmail.com>
2022-08-26 16:04:21 +0200

    correclty setup empty url

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', views.index, name='home'),
+    path('', views.index, name='home'),
 
     path('lettings/', include('lettings.urls')),
     path('profiles/', include('profiles.urls')),
