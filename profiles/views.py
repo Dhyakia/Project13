233c5668482958d99ffc98f97f6dea49a759a785
Rimonteil <nardiotencha@gmail.com>
2022-09-05 18:12:34 +0200

    test

--- profiles/views.py
+++ profiles/views.py
@@ -13,3 +13,4 @@ def profile(request, username):
     profile = Profile.objects.get(user__username=username)
     context = {'profile': profile}
     return render(request, 'profiles/profile.html', context)
+
